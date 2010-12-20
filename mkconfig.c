405be3b9346dbd5adcf02ba19bad334ac80bbdb0
Stephan Plepelits <skunk@xover.mud.at>
2010-12-20 18:00:59 +0100

    Save a struct for each file, write config in the end

--- mkconfig.c
+++ mkconfig.c
@@ -4,15 +4,19 @@
 #include <sys/types.h>
 #include <regex.h>
 
+#define MAX_SHAPEFILES 32
+#define BUFSIZE 1024
+
 regex_t r_file;
-int file_id;
 struct shape_file {
   int	id;
   int format;
   char* name;
   int ncolumns;
 };
-struct shape_file current_shape_file;
+int file_count;
+struct shape_file *all[MAX_SHAPEFILES];
+struct shape_file *current_shape_file;
 
 void setup_regex() {
   int ret;
@@ -23,56 +27,59 @@ void setup_regex() {
   }
 }
 
-void write_old_file() {
-  if(current_shape_file.id==-1)
-    return;
+void write_files() {
+  int i;
 
-  printf("shapefile_new(%d, %i, \"%s\", X", current_shape_file.id, current_shape_file.format, current_shape_file.name);
+  for(i=0; i<file_count; i++) {
+    current_shape_file=all[i];
+    printf("shapefile_new(%d, %i, \"%s\", X", current_shape_file->id, current_shape_file->format, current_shape_file->name);
+  }
 }
+
 
 void parse_line(char *row) {
   regmatch_t matches[10];
-  char tmp[1024];
+  char tmp[BUFSIZE];
   int tmpi;
 
   if(!(regexec(&r_file, row, 3, matches, 0))) {
-    write_old_file();
-    current_shape_file.id=++file_id;
+    current_shape_file=malloc(sizeof(struct shape_file));
+    all[file_count]=current_shape_file;
+    current_shape_file->id=file_count++;
 
     strncpy(tmp, row+matches[1].rm_so, matches[1].rm_eo-matches[1].rm_so);
     tmp[matches[1].rm_eo-matches[1].rm_so]='\0';
     tmpi=
-      (strcmp(tmp, "point")?0:
-       strcmp(tmp, "line")?1:
-       strcmp(tmp, "polygon")?2:-1);
+      (!strcmp(tmp, "point")?0:
+       !strcmp(tmp, "line")?1:
+       !strcmp(tmp, "polygon")?2:-1);
     if(tmpi==-1) {
       fprintf(stderr, "Error parsing type of shapefile, must be point, line or polygon: %s\n", tmp);
       exit(1);
     }
-    current_shape_file.format=tmpi;
+    current_shape_file->format=tmpi;
 
     strncpy(tmp, row+matches[2].rm_so, matches[2].rm_eo-matches[2].rm_so);
     tmp[matches[2].rm_eo-matches[2].rm_so]='\0';
-    current_shape_file.name=malloc(strlen(tmp)+1);
-    strcpy(current_shape_file.name, tmp);
+    current_shape_file->name=malloc(strlen(tmp)+1);
+    strcpy(current_shape_file->name, tmp);
   }
 }
 
 void main() {
   setup_regex();
 
-  file_id=-1;
-  current_shape_file.id=-1;
+  file_count=0;
 
   FILE *f;
-  char row[1024];
+  char row[BUFSIZE];
   f=fopen("osm2shp.cfg", "r");
 
-  while(fgets(row, 1024, f)) {
+  while(fgets(row, BUFSIZE, f)) {
     printf("Read: %s", row);
 
     parse_line(row);
   }
 
-  write_old_file();
+  write_files();
 }
