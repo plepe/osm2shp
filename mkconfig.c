50fa2b9e73731e7de0c3c62c2e2d5bd3cf5cf085
Stephan Plepelits <skunk@xover.mud.at>
2010-12-20 19:55:45 +0100

    Filter objects to put into shapefiles by 'where'

--- mkconfig.c
+++ mkconfig.c
@@ -12,6 +12,7 @@
 regex_t r_file;
 regex_t r_column;
 regex_t r_key;
+regex_t r_where;
 
 struct shape_column {
   int type;
@@ -26,6 +27,8 @@ struct shape_file {
   char* name;
   int ncolumns;
   struct shape_column *columns[MAX_COLUMNS];
+  int nwheres;
+  char *wheres[MAX_KEYS];
 };
 int file_count;
 struct shape_file *all[MAX_SHAPEFILES];
@@ -44,6 +47,11 @@ void setup_regex() {
   }
 
   if((ret=regcomp(&r_column, "^column ([a-z]+) \"([^\"]*)\"", REG_EXTENDED))) {
+    fprintf(stderr, "Error compiling regular expression: %d\n", ret);
+    exit(1);
+  }
+
+  if((ret=regcomp(&r_where, "^\\w*where", REG_EXTENDED))) {
     fprintf(stderr, "Error compiling regular expression: %d\n", ret);
     exit(1);
   }
@@ -73,13 +81,17 @@ void write_files_setup() {
 }
 
 void write_files_parse(int format) {
-  int i, col_i;
+  int i, col_i, where_i;
 
   for(i=0; i<file_count; i++) {
     current_shape_file=all[i];
 
     if(current_shape_file->format!=format)
       continue;
+
+    for(where_i=0; where_i<current_shape_file->nwheres; where_i++) {
+	printf("\tconst char *where%d = g_hash_table_lookup(current_tags, \"%s\");\n", where_i, current_shape_file->wheres[where_i]);
+    }
 
     for(col_i=0; col_i<current_shape_file->ncolumns; col_i++) {
       struct shape_column *column=current_shape_file->columns[col_i];
@@ -97,6 +109,34 @@ void write_files_parse(int format) {
       }
     }
 
+/*    if(current_shape_file->nwheres==0) {
+      int col_i;
+      printf("\tif (");
+
+      if(current_shape_file->ncolumns>1)
+	printf("(col0)");
+      for(col_i=1; col_i<current_shape_file->ncolumns; col_i++) {
+	printf(" || (col%d)", col_i);
+      }
+
+      printf(") {\n");
+    } */
+    if(current_shape_file->nwheres>0) {
+      int where_i;
+
+      printf("\tif (");
+
+      printf("(where%d)", 0);
+      for(where_i=1; where_i<current_shape_file->nwheres; where_i++) {
+	printf(" || (where%d)", where_i);
+      }
+
+      printf(" ) {\n");
+    }
+    else {
+      printf("\t{");
+    }
+
     printf("\tshapefile_add_%s(%i, current_id", OSM_TYPE[current_shape_file->format], current_shape_file->id);
     for(col_i=0; col_i<current_shape_file->ncolumns; col_i++) {
       struct shape_column *column=current_shape_file->columns[col_i];
@@ -112,7 +152,7 @@ void write_files_parse(int format) {
 	  break;
       }
     }
-    printf(");\n\n");
+    printf(");\n\t}\n\n");
   }
 }
 
@@ -127,6 +167,7 @@ void parse_line(char *row) {
     all[file_count]=current_shape_file;
     current_shape_file->id=file_count++;
     current_shape_file->ncolumns=0;
+    current_shape_file->nwheres=0;
 
     strncpy(tmp, row+matches[1].rm_so, matches[1].rm_eo-matches[1].rm_so);
     tmp[matches[1].rm_eo-matches[1].rm_so]='\0';
@@ -176,6 +217,20 @@ void parse_line(char *row) {
       column->keys[column->nkeys]=malloc(strlen(tmp)+1);
       strcpy(column->keys[column->nkeys], tmp);
       column->nkeys++;
+
+      row=row+matches[0].rm_eo+1;
+    }
+  }
+
+  if(!(regexec(&r_where, row, 1, matches, 0))) {
+    row=row+matches[0].rm_eo+1;
+
+    while(!(regexec(&r_key, row, 2, matches, 0))) {
+      strncpy(tmp, row+matches[1].rm_so, matches[1].rm_eo-matches[1].rm_so);
+      tmp[matches[1].rm_eo-matches[1].rm_so]='\0';
+      current_shape_file->wheres[current_shape_file->nwheres]=malloc(strlen(tmp)+1);
+      strcpy(current_shape_file->wheres[current_shape_file->nwheres], tmp);
+      current_shape_file->nwheres++;
 
       row=row+matches[0].rm_eo+1;
     }
